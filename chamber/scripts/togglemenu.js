6e61b23802e235f9db00efe0acb8c31b596a264b
Kaler Sappington <kaler.sappington@gmail.com>
2024-05-25 16:43:46 -0600

    updated some things

--- chamber/scripts/togglemenu.js
+++ chamber/scripts/togglemenu.js
@@ -25,3 +25,39 @@ window.addEventListener('resize', function() {
         hamburger.innerHTML = "☰";
     }
 });
+const modeButton = document.querySelector(".mode");
+const main = document.querySelector("main");
+const section = document.querySelectorAll("section");
+const body = document.querySelector("body");
+const h2 = document.querySelector("h2");
+const p = document.querySelectorAll("p");
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+        body.style.background = "#fff";
+        h2.style.color = "#fff";
+        section.forEach(section => {
+            section.style.background = "#333";
+            section.style.color = 'white';
+          });
+        p.forEach(p => {
+            p.style.color = 'white';
+          });
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#ddd";
+        body.style.background = "#ddd";
+		main.style.color = "#000";
+        h2.style.color = "#000";
+		section.forEach(section => {
+            section.style.background = "#fff";
+            section.style.color = 'black';
+          });
+        p.forEach(p => {
+            p.style.color = 'black';
+          });
+        modeButton.textContent = "🕶️";
+	}
+});
+
